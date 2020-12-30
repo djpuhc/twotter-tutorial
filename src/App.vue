dc50bb446e7eed84018496ed3a0538be18ddce02
djp <doug_pomerenke@uhc.com>
2020-12-30 11:15:05 -0600

    Intitial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="app">
+  Welcome {{user.username}} - The current time is - {{currentTime}}
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'djp'
+      }
+    }
+  },
+  computed:{
+    currentTime(){
+      return Date();
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
